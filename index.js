158e137f07786d59a916783e8877e8a7288c9824
Chris Geihsler <chris.geihsler@auth0.com>
2020-07-30 08:48:23 -0400

    My custom prompt

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.urlencoded({
+    extended: true
+  }))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/foo', (req, res) => res.render('pages/foo'))
+  .get('/my-custom-prompt', (req, res) => {
+    const state = req.query.state;
+
+    res.render('pages/foo', { state });
+  })
+  .post('/my-custom-prompt', (req, res) => {
+    console.log(req.body)
+    const state = req.body.state;
+
+    console.log("Redirecting with state ", state);
+
+    res.redirect(`https://seejee-node-12.stage.auth0.com/continue?state=${state}`);
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
